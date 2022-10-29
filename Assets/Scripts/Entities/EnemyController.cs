47b0ca31f98985a6393b5bea159ddec7feb3ace2
Adrian <adriporcel90@gmail.com>
2022-10-29 22:38:46 +0200

    Random sprite and collision for enemy on Awake

--- Assets/Scripts/Entities/EnemyController.cs
+++ Assets/Scripts/Entities/EnemyController.cs
@@ -2,12 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class EnemyController : MonoBehaviour
 {
     [Header("Enemy base stats")]
     [SerializeField] uint health;
     [SerializeField] uint size;
     [SerializeField] float speed;
+
+    private void Awake()
+    {
+        LoadSpriteAndCollision();
+    }
+
+    private void LoadSpriteAndCollision()
+    {
+        Sprite[] spriteArray = Resources.LoadAll<Sprite>("Sprites/Asteroids/");
+        int spriteSelected = Random.Range(0, spriteArray.Length);
+
+        GetComponent<SpriteRenderer>().sprite = spriteArray[spriteSelected];
+
+        gameObject.AddComponent<PolygonCollider2D>();
+        GetComponent<PolygonCollider2D>().isTrigger = true;
+    }
 
     void OnTriggerEnter(Collider other)
     {
