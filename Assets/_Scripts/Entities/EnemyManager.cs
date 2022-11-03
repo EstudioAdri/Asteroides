83510ea02fc8ce19b8a88ef294c8d2920b149dc2
avallejoganga <114919382+avallejoganga@users.noreply.github.com>
2022-11-03 17:49:40 +0100

    Teleports Done for asteroids and proyectiles

--- Assets/_Scripts/Entities/EnemyManager.cs
+++ Assets/_Scripts/Entities/EnemyManager.cs
@@ -44,4 +44,6 @@ public class EnemyManager : MonoBehaviour
     {
         enemyCount = inst.Enemies.Count;
     }
+
+    
 }
--- Assets/_Scripts/Entities/ProyectileController.cs
+++ Assets/_Scripts/Entities/ProyectileController.cs
@@ -25,4 +25,42 @@ public class ProyectileController : MonoBehaviour
     {
         
     }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject collidedWith = collision.gameObject;
+        Vector2 teleport;
+        switch (collidedWith.tag)
+        {
+            case "Wall X":
+                teleport = this.transform.position;
+                teleport.x *= -1;
+                if (teleport.x < 0)
+                {
+                    teleport.x += 0.15f;
+                }
+                else
+                {
+                    teleport.x -= 0.15f;
+                }
+                this.transform.position = teleport;
+                break;
+            case "Wall Y":
+                teleport = this.transform.position;
+                teleport.y *= -1;
+                if (teleport.y < 0)
+                {
+                    teleport.y += 0.15f;
+                }
+                else
+                {
+                    teleport.y -= 0.15f;
+                }
+                this.transform.position = teleport;
+                break;
+        }
+        
+    }
+
 }
